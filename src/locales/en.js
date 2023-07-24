a0b04c7ef6832129ca2bfa98983ce6e96d6b2a04
Kurbonov P <kroritord@bk.ru>
2023-07-24 23:48:18 +0300

    add en.js

---
+++ src/locales/en.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    header: 'RSS-agregator',
+    lead: 'Receive notifications of new content in a convenient format.',
+    urlInput: 'RSS link',
+    addButton: 'Add',
+    empty: 'Fill out this fild',
+    invalidURLFeedback: 'Link must be a valid URL',
+    alreadyExistsFeedback: 'RSS already exists',
+    goodFeedback: 'RSS uploaded successfully',
+    key_one: '{{count}} click',
+    key_other: '{{count}} clicks',
+  },
+};
