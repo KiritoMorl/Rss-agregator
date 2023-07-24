702dafaab3546fff74b5c93c99b1953c9d8dcfc9
Kurbonov P <kroritord@bk.ru>
2023-07-24 23:48:34 +0300

    add ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    header: 'RSS-агрегатор',
+    lead: 'Получайте уведомления о новом контенте в удобном формате.',
+    urlInput: 'Ссылка RSS',
+    addButton: 'Добавить',
+    empty: 'Заполните это поле',
+    invalidURLFeedback: 'Ссылка должна быть валидным URL',
+    alreadyExistsFeedback: 'RSS уже существует',
+    goodFeedback: 'RSS успешно загружен',
+    key_one: '{{count}} click',
+    key_other: '{{count}} clicks',
+  },
+};
