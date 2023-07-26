36dad23f3371619df5341d72edfe7431d9d1f7b4
Kurbonov P <kroritord@bk.ru>
2023-07-26 21:40:56 +0300

    add posts create and little fix elements

--- src/application.js
+++ src/application.js
@@ -4,7 +4,28 @@ import onChange from 'on-change';
 import { setLocale, string } from 'yup';
 import render from './render.js';
 import resources from './locales/index.js';
+import parser from './utils/parser.js';
 
+const getAllOriginsResponse = (url) => {
+  const allOriginsLink = 'https://allorigins.hexlet.app/get';
+
+  const workingUrl = new URL(allOriginsLink);
+  workingUrl.searchParams.set('disableCache', 'true');
+  workingUrl.searchParams.set('url', url);
+
+  return axios.get(workingUrl);
+};
+
+let count = -1;
+const takeNewFeedId = (feed) => {
+  count += 1;
+  feed.id = count;
+};
+let postsCount = -1;
+const takeNewPostId = (post) => {
+  postsCount += 1;
+  post.id = postsCount;
+};
 
 export default async () => {
   setLocale({
@@ -19,13 +40,14 @@ export default async () => {
   });
   const initialState = {
     form: {
-      error: '',
+      error: 'default',
       status: 'filling',
       fields: {
         url: '',
       },
     },
     feeds: [],
+    posts: [],
   };
 
   const i18n = i18next.createInstance();
@@ -44,6 +66,8 @@ export default async () => {
     header: document.querySelector('h1'),
     h2: document.querySelector('.lead'),
     urlLabel: document.querySelector('#urlLabel'),
+    feedsColumn: document.querySelector('.feeds'),
+    postsColumn: document.querySelector('.posts'),
   };
 
   const state = onChange(
@@ -62,15 +86,41 @@ export default async () => {
     schema.validate(state.form.fields.url)
       .then(() => {
         state.form.status = 'sending';
+        return getAllOriginsResponse(state.form.fields.url);
       })
-      .then(() => {
+      .then((body) => {
+        const parsedContent = parser(body.data.contents);
+        const posts = parsedContent.querySelectorAll('item');
+        posts.forEach((post) => {
+          const title = post.querySelector('title').textContent;
+          const description = post.querySelector('description').textContent;
+          const link = post.querySelector('link').textContent;
+          const somePost = {
+            id: 0,
+            title,
+            description,
+            link,
+          };
+          takeNewPostId(somePost);
+          console.log(somePost);
+          elements.postsColumn.innerHTML = '';
+          state.posts.push(somePost);
+        });
+        //console.log(posts)
+        // console.log(parsedContent.querySelector('title').textContent)
         const feed = {
           id: 0,
           link: state.form.fields.url,
+          description: parsedContent.querySelector('title').textContent,
+          title: parsedContent.querySelector('description').textContent,
         };
+        takeNewFeedId(feed);
+        //console.log(parsedContent);
+        elements.feedsColumn.innerHTML = '';
         state.feeds.push(feed);
         console.log(getFeedUrls);
         state.form.fields.url = '';
+        state.form.error = '';
       })
       .catch((error) => {
         const message = error.message ?? 'default';
